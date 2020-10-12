714ab0be4b6aa4636f181908701e8db4dc5dbc1b
Matheus <rsantos.matheus@outlook.com>
2020-10-12 16:35:01 -0300

    Exception handling - update

--- src/main/java/com/SpringbootJPA/Hibernate/course/services/UserService.java
+++ src/main/java/com/SpringbootJPA/Hibernate/course/services/UserService.java
@@ -2,6 +2,8 @@ package com.SpringbootJPA.Hibernate.course.services;
 
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -33,21 +35,25 @@ public class UserService {
 		return repository.save(obj);
 	}
 
-	public void delete (Long id) {
+	public void delete(Long id) {
 		try {
-		repository.deleteById(id);
-		}
-		catch (EmptyResultDataAccessException e) {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
 			throw new ResourceNotFoundException(id);
-		}catch (DataIntegrityViolationException e) {
+		} catch (DataIntegrityViolationException e) {
 			throw new DatabaseException(e.getMessage());
 		}
 	}
 
 	public User update(Long id, User obj) {
+		try {
 		User entity = repository.getOne(id);
 		updateData(entity, obj);
 		return repository.save(entity);
+		}
+		catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
