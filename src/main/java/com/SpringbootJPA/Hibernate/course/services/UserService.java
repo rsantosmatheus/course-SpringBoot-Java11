5515376d1489f4ece2b7c3aba0d16ca22713ed20
Matheus <rsantos.matheus@outlook.com>
2020-10-12 16:15:13 -0300

    Exception handling - findById

--- src/main/java/com/SpringbootJPA/Hibernate/course/services/UserService.java
+++ src/main/java/com/SpringbootJPA/Hibernate/course/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.SpringbootJPA.Hibernate.course.entities.User;
 import com.SpringbootJPA.Hibernate.course.repositories.UserRepository;
+import com.SpringbootJPA.Hibernate.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,8 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 
 	}
 	
