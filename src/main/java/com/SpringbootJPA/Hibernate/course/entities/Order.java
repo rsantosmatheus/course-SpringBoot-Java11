8b150e645572b57dc0ad64709a064fa731498eef
Matheus <rsantos.matheus@outlook.com>
2020-10-12 09:50:40 -0300

    OrderStatus Enums

--- src/main/java/com/SpringbootJPA/Hibernate/course/entities/Order.java
+++ src/main/java/com/SpringbootJPA/Hibernate/course/entities/Order.java
@@ -11,8 +11,8 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import com.SpringbootJPA.Hibernate.course.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonFormatTypes;
 
 @Entity
 @Table(name = "tb_order")
@@ -26,7 +26,7 @@ public class Order implements Serializable {
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
 
-//	private OrderStatus orderStatus;
+	private Integer orderStatus;
 
 	@ManyToOne
 	@JoinColumn(name = "client_id")
@@ -35,10 +35,10 @@ public class Order implements Serializable {
 	public Order() {
 	}
 
-	public Order(Long id, Instant moment, User client) {
+	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
 		this.id = id;
 		this.moment = moment;
-		// this.orderStatus = orderStatus;
+		setOrderStatus(orderStatus);
 		this.client = client;
 	}
 
@@ -58,12 +58,15 @@ public class Order implements Serializable {
 		this.moment = moment;
 	}
 
-	/*
-	 * public OrderStatus getOrderStatus() { return orderStatus; }
-	 * 
-	 * public void setOrderStatus(OrderStatus orderStatus) { this.orderStatus =
-	 * orderStatus; }
-	 */
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if (orderStatus != null) {
+			this.orderStatus = orderStatus.getCode();
+		}
+	}
 
 	public User getClient() {
 		return client;
